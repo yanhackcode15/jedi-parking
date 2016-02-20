2b10f2cbfd14369cdce63d18b00a2fdf57d28c7d
Yan Hu <iamyanhu@gmail.com>
2016-02-20 15:57:06 +0000

    Added a version to application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module JediParking
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.version = '1.0'
   end
 end
