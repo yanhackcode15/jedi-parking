052cc718be9e801bd6865a12a37b54893046331e
Yan Hu <iamyanhu@gmail.com>
2016-02-24 17:37:05 +0000

    Fixed faye host address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  def index
+  	@url = 'http://jedi-parking.herokuapp.com:'+$PORT+'/faye/client.js'
+  end
 end
